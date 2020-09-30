a20fb462bfc1f7348c8aabb0d44aa077f60690fa
Cristian Padilla <Cristancho2011@gmail.com>
2020-09-29 23:35:39 -0500

    Implementando hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState ,useEffect}from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -8,37 +8,47 @@ import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss';
 
-const App = () =>(
-    <div className= "App">
-        <Header></Header>
-        
-        <Search></Search>
-        
-        <Categories title =" Mi lista">
-            <Carousell>
-                <CarousellItem></CarousellItem>
-                <CarousellItem></CarousellItem>
-                <CarousellItem></CarousellItem>
-                <CarousellItem></CarousellItem>
-                <CarousellItem></CarousellItem>
-                <CarousellItem></CarousellItem>
-                <CarousellItem></CarousellItem>
-               
-            </Carousell>
-        </Categories>
-        <Categories title =" Tendecias">
-            <Carousell>
-                <CarousellItem></CarousellItem>
-                <CarousellItem></CarousellItem>   
-            </Carousell>
-        </Categories>
-        <Categories title =" Originales de platzi video">
-            <Carousell>
-                <CarousellItem></CarousellItem>   
-            </Carousell>
-        </Categories>
-        <Footer></Footer>
-    </div>
-
-);
+const App = () =>{
+    const [ videos , setVideos] = useState([]);
+    
+    useEffect(()=>{
+        //prender servidor --json-server initialState.json  
+        fetch("http://localhost:3000/initalState")
+            .then(response => response.json())
+            .then(data => setVideos(data));
+    },[]);
+    console.log(videos)
+    return(
+        <div className= "App">
+            <Header></Header>   
+            
+            <Search></Search>
+            
+            <Categories title =" Mi lista">
+                <Carousell>
+                    <CarousellItem></CarousellItem>
+                    <CarousellItem></CarousellItem>
+                    <CarousellItem></CarousellItem>
+                    <CarousellItem></CarousellItem>
+                    <CarousellItem></CarousellItem>
+                    <CarousellItem></CarousellItem>
+                    <CarousellItem></CarousellItem>
+                
+                </Carousell>
+            </Categories>
+            <Categories title =" Tendecias">
+                <Carousell>
+                    <CarousellItem></CarousellItem>
+                    <CarousellItem></CarousellItem>   
+                </Carousell>
+            </Categories>
+            <Categories title =" Originales de platzi video">
+                <Carousell>
+                    <CarousellItem></CarousellItem>   
+                </Carousell>
+            </Categories>
+            <Footer></Footer>
+        </div>
+    );
+}
 export default App;
